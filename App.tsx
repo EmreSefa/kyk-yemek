6c6dc60d650357df8b191281f49ca2f3ef90022b
EmreSefa <emreskadirhan@gmail.com>
2025-03-25 22:39:12 +0300

    Update project configuration files

--- App.tsx
+++ App.tsx
@@ -1,21 +1,2 @@
-import "react-native-url-polyfill/auto";
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
-import AppNavigator from "./app/navigation/AppNavigator";
-import { AuthProvider } from "./app/hooks/useAuth";
-
-export default function App() {
-  const colorScheme = useColorScheme();
-  const isDarkMode = colorScheme === "dark";
-
-  return (
-    <AuthProvider>
-      <SafeAreaProvider>
-        <StatusBar style={isDarkMode ? "light" : "dark"} />
-        <AppNavigator />
-      </SafeAreaProvider>
-    </AuthProvider>
-  );
-}
+import App from "./app/App";
+export default App;
